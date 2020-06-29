1391d77282569c96c47660a4562a2e73a7c4677d
Marius Tobiassen <mariust@runway.com.au>
2020-06-29 17:02:15 +1000

    22052020

--- src/test/java/Pages/ContactUs.java
+++ src/test/java/Pages/ContactUs.java
@@ -14,13 +14,18 @@ public class ContactUs {
     public void fillInForm(){
         WebElement firstNameField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id280']")));
         WebElement lastNameField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id282']")));
-        WebElement eMailField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='']")));
-        WebElement phoneField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='']")));
-        WebElement inquiryContentsField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='']")));
-
-
+        WebElement eMailField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id284']")));
+        WebElement phoneField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id286']")));
+        WebElement inquiryContentsField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id298']")));
+        WebElement inquiryTitleDropdown = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id288']")));
+        WebElement inquiryCategory1Dropdown = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id292']")));
+        WebElement inquiryCategory2Dropdown = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:subcategory2uq']")));
 
 
         firstNameField.sendKeys("John");
+        lastNameField.sendKeys("Test");
+        eMailField.sendKeys("johntest@johntest.com");
+        phoneField.sendKeys("0123456789");
+        inquiryContentsField.sendKeys("Inquiry content goes here");
     }
 }
