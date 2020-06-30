603bd5536793f460a58e7395ea4cb9aeabe1b790
Marius Tobiassen <mariust@runway.com.au>
2020-06-30 17:02:30 +1000

    30062020

--- src/test/java/Pages/ContactUs.java
+++ src/test/java/Pages/ContactUs.java
@@ -12,14 +12,14 @@ public class ContactUs {
     public ContactUs (WebDriver aDriver) {driver = aDriver;}
 
     public void fillInForm(){
-        WebElement firstNameField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id280']")));
-        WebElement lastNameField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id282']")));
-        WebElement eMailField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id284']")));
-        WebElement phoneField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id286']")));
-        WebElement inquiryContentsField = new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id298']")));
-        WebElement inquiryTitleDropdown = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id288']")));
-        WebElement inquiryCategory1Dropdown = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id292']")));
-        WebElement inquiryCategory2Dropdown = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:subcategory2uq']")));
+        WebElement firstNameField = new WebDriverWait(driver,40).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id280']")));
+        WebElement lastNameField = new WebDriverWait(driver,40).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id282']")));
+        WebElement eMailField = new WebDriverWait(driver,40).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id284']")));
+        WebElement phoneField = new WebDriverWait(driver,40).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='j_id0:j_id274:j_id286']")));
+        WebElement inquiryContentsField = new WebDriverWait(driver,40).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("textarea[name='j_id0:j_id274:j_id298']")));
+        WebElement inquiryTitleDropdown = new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id288']")));
+        WebElement inquiryCategory1Dropdown = new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id292']")));
+        WebElement inquiryCategory2Dropdown = new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:subcategory2uq']")));
 
 
         firstNameField.sendKeys("John");
