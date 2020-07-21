d4457bd15ab5a0e66fc65f718bff9373a6b10db5
Marius Tobiassen <mariust@runway.com.au>
2020-07-21 16:50:02 +1000

    21072020

--- src/test/java/Pages/ContactUs.java
+++ src/test/java/Pages/ContactUs.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class ContactUs {
     private WebDriver driver;
@@ -21,11 +26,26 @@ public class ContactUs {
         WebElement inquiryCategory1Dropdown = new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:j_id292']")));
         WebElement inquiryCategory2Dropdown = new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(By.cssSelector("select[name='j_id0:j_id274:subcategory2uq']")));
 
-
         firstNameField.sendKeys("John");
         lastNameField.sendKeys("Test");
         eMailField.sendKeys("johntest@johntest.com");
         phoneField.sendKeys("0123456789");
+/*
+
+        List<WebElement> inquiryTitleOptions = driver.findElements(By.cssSelector(".is-error option[value]"));
+        Random rand = new Random();
+        int selectedInquiryTitleOption = rand.nextInt(inquiryTitleOptions.size());
+        inquiryTitleDropdown = inquiryTitleOptions.get(selectedInquiryTitleOption);
+        inquiryTitleDropdown.click();
+*/
+        Select selectTitle = new Select(inquiryTitleDropdown);
+        selectTitle.selectByVisibleText("Compliment");
+        Select selectCategory1 = new Select(inquiryCategory1Dropdown);
+        selectCategory1.selectByVisibleText("Product");
+        inquiryCategory2Dropdown = new WebDriverWait(driver, 40).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Quality")));
+        Select selectCategory2 = new Select(inquiryCategory2Dropdown);
+        selectCategory2.selectByVisibleText("Quality");
         inquiryContentsField.sendKeys("Inquiry content goes here");
+
     }
 }
