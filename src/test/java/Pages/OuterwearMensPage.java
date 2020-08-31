f3c07586ca096e0561b89dfc91c6bb1a8a35da41
Marius Tobiassen <mariust@runway.com.au>
2020-08-31 15:39:35 +1000

    31082020

--- src/test/java/Pages/OuterwearMensPage.java
+++ src/test/java/Pages/OuterwearMensPage.java
@@ -9,11 +9,13 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 public class OuterwearMensPage {
 
-    private WebDriver driver:
+    private WebDriver driver;
 
     public OuterwearMensPage(WebDriver aDriver){driver = aDriver;}
 
@@ -37,6 +39,22 @@ public class OuterwearMensPage {
             Assert.assertThat(driver.getTitle(), Is.is("VIEW ALL OUTERWEAR - OUTERWEAR - MEN  | UNIQLO"));
     }
     public void selectOuterwearProduct(){
-            
+         WebElement outerwear = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("")));
+
+         //Select product
+        List<WebElement> products = driver.findElements(By.xpath("//li[contains(@class,'item') and not (contains(@class, 'menu'))] "));
+        int numberOfProducts = products.size();
+        System.out.println(numberOfProducts);
+        Random rand = new Random();
+        int randomProduct = rand.nextInt(numberOfProducts);
+
+        //Select random available size and available color
+        List<WebElement> colors = driver.findElements(By.cssSelector(".selectAttr [color]"));
+        int numberOfColors = colors.size();
+        System.out.println(numberOfColors);
+
+
         }
+
+    }
 }
